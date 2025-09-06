13ba1d6e96d3a38f08c320c0bada3f9745aae48d
Shomari <nyusternie@sdot.io>
2025-09-06 15:35:14 -0400

    Remove blog (from config).

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,21 +50,6 @@ const config = {
                     editUrl:
                         'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                 },
-                blog: {
-                    showReadingTime: true,
-                    feedOptions: {
-                        type: ['rss', 'atom'],
-                        xslt: true,
-                    },
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                    // Useful options to enforce blogging best practices
-                    onInlineTags: 'warn',
-                    onInlineAuthors: 'warn',
-                    onUntruncatedBlogPosts: 'warn',
-                },
                 theme: {
                     customCss: './src/css/custom.css',
                 },
@@ -90,7 +75,6 @@ const config = {
                         position: 'left',
                         label: 'Quick Start Guide',
                     },
-                    // {to: '/blog', label: 'Blog', position: 'left'},
                     {
                         href: 'https://yappr.world/roadmap',
                         label: 'Roadmap',
@@ -131,8 +115,8 @@ const config = {
                         title: 'More',
                         items: [
                             {
-                                label: 'Blog',
-                                to: '/blog',
+                                label: 'More coming soon',
+                                to: '/',
                             },
                             {
                                 label: 'GitHub',
